ee49e57d494ef64725db7c2e90f97dd2f972dfe6
khadijamansoor <khadijamansoor47@gmail.com>
2025-09-30 11:22:44 +0500

    first commit

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,4 +1,4 @@
-import Header from '../components/Header/Header'
+import Header from '../../components/Header/Header'
 
 import Showcase from './components/Showcase/Showcase'
 import MoreLikeThis from './components/MoreLikeThis/MoreLikeThis'
